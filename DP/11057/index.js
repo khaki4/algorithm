c38b087a260929ae955c4a68d1152f76cfa23d28
jiwonjung <4dakto@gmail.com>
2022-07-16 19:00:33 +0900

    u

--- DP/11057/index.js
+++ DP/11057/index.js
@@ -1,28 +1,19 @@
 const fs = require("fs");
 const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
-const [limit, ...inputs] = fs
-  .readFileSync(filePath)
-  .toString()
-  .trim()
-  .split("\n");
-
-const n = limit * 1;
-const a = inputs.map((v) => v.split(" ").map(Number));
-const d = Array.from(Array(n + 1), () => Array(3).fill(1001));
-d[0] = [0, 0, 0];
+const n = fs.readFileSync(filePath).toString().trim() * 1;
+const mod = 10007;
+const d = Array.from(Array(n + 1), () => Array(10).fill(0));
+d[1] = Array(10).fill(1);
 
 for (let i = 1; i <= n; i++) {
-  for (let j = 0; j <= 2; j++) {
-    if (j === 0) {
-      d[i][j] = Math.min(d[i - 1][1], d[i - 1][2]) + a[i - 1][j];
-    }
-    if (j === 1) {
-      d[i][j] = Math.min(d[i - 1][0], d[i - 1][2]) + a[i - 1][j];
-    }
-    if (j === 2) {
-      d[i][j] = Math.min(d[i - 1][0], d[i - 1][1]) + a[i - 1][j];
+  for (let j = 0; j <= 9; j++) {
+    for (let k = 0; k <= j; k++) {
+      d[i][j] += d[i - 1][k];
+      d[i][j] %= mod;
     }
   }
 }
 
-console.log(Math.min(...d[n]));
+const res = d[n].reduce((a, b) => (a + b) % mod, 0);
+
+console.log(res);
