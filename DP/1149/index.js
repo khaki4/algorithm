7eaad88292e157c5de03e790991b8bf692034657
jiwonjung <4dakto@gmail.com>
2022-07-16 16:41:47 +0900

    update

--- DP/1149/index.js
+++ DP/1149/index.js
@@ -3,6 +3,7 @@ const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
 const [_, ...inputs] = fs
   .readFileSync(filePath)
   .toString()
+  .trim()
   .split("\n")
   .map(Number);
 
@@ -12,12 +13,10 @@ d[1] = 1;
 d[2] = 2;
 d[3] = 4;
 
-let res = "";
-for (let i = 4; i <= 1000000; i++) {
-  d[i] = d[i - 1] + d[i - 2] + d[i - 3];
-  d[i] %= mod;
-}
-
-inputs.forEach((v) => (res += `${d[v]}\n`));
-
-console.log(res);
+inputs.map(Number).forEach((n) => {
+  for (let i = 4; i <= n; i++) {
+    d[i] = d[i - 1] + d[i - 2] + d[i - 3];
+    d[i] %= mod;
+  }
+  console.log(d[n]);
+});
