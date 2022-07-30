5523de18034de0c8c487c4bd8d9b7aeedd3b4343
khaki4 <4dakto@gmail.com>
2022-07-30 12:11:28 +0900

    490

---
+++ DP/490/index.js
@@ -0,0 +1,17 @@
+const fs = require("fs");
+const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
+// const n = fs.readFileSync(filePath).toString().trim() * 1;
+const [n, k] = `20 2`.toString().trim().split(/\s/).map(Number);
+const d = Array.from(Array(k + 1), () => Array(n + 1).fill(0));
+
+d[0][0] = 1;
+for (let i = 1; i <= k; i++) {
+  for (let j = 0; j <= n; j++) {
+    for (let l = 0; l <= j; l++) {
+      d[i][j] += d[i - 1][j - l];
+      d[i][j] %= 1000000000;
+    }
+  }
+}
+
+console.log(d[k][n]);
