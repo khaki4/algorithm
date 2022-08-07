fe1ecefea20a3f1dd3b32a9b4c8f0c3e55c56fcc
khaki4 <4dakto@gmail.com>
2022-08-07 20:36:47 +0900

    1107

--- brute_force/1107/index.js
+++ brute_force/1107/index.js
@@ -1,22 +1,135 @@
 const fs = require("fs");
 const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
-const inputs = fs.readFileSync(filePath).toString().trim().split("\n");
+const [c, ...inputs] = fs.readFileSync(filePath).toString().trim().split("\n");
 
-const channel = inputs[0] * 1;
-const brokenNumber = (inputs[2] ?? "-1").split(" ");
+const [n, m] = c.split(/\s/).map(Number);
+const a = inputs.map((v) => v.split(/\s/).map(Number));
 
-let ans = Math.abs(channel - 100);
-for (let i = 0; i <= 1000000; i++) {
-  if (isBroken(i)) continue;
-
-  ans = Math.min(ans, Math.abs(channel - i) + i.toString().length);
+let ans = 0;
+for (let i = 0; i < n; i++) {
+  for (let j = 0; j < m; j++) {
+    // a
+    if (j + 3 < m) {
+      ans = Math.max(ans, a[i][j] + a[i][j + 1] + a[i][j + 2] + +a[i][j + 3]);
+    }
+    // b
+    if (i + 3 < n) {
+      ans = Math.max(ans, a[i][j] + a[i + 1][j] + a[i + 2][j] + a[i + 3][j]);
+    }
+    // c
+    if (i + 1 < n && j + 1 < m) {
+      ans = Math.max(
+        ans,
+        a[i][j] + a[i + 1][j] + a[i][j + 1] + a[i + 1][j + 1]
+      );
+    }
+    // d
+    if (i + 2 < n && j + 1 < m) {
+      ans = Math.max(
+        ans,
+        a[i][j] + a[i + 1][j] + a[i + 2][j] + a[i + 2][j + 1]
+      );
+    }
+    // e
+    if (i + 2 < n && j + 1 < m) {
+      ans = Math.max(
+        ans,
+        a[i + 2][j] + a[i][j + 1] + a[i + 1][j + 1] + a[i + 2][j + 1]
+      );
+    }
+    // f
+    if (i + 1 < n && j + 2 < m) {
+      ans = Math.max(
+        ans,
+        a[i][j] + a[i + 1][j] + a[i + 1][j + 1] + a[i + 1][j + 2]
+      );
+    }
+    // g
+    if (i + 1 < n && j + 2 < m) {
+      ans = Math.max(ans, a[i][j] + a[i + 1][j] + a[i][j + 1] + a[i][j + 2]);
+    }
+    // h
+    if (i + 1 < n && j + 2 < m) {
+      ans = Math.max(
+        ans,
+        a[i + 1][j] + a[i + 1][j + 1] + a[i + 1][j + 2] + a[i][j + 1]
+      );
+    }
+    // i
+    if (i + 1 < n && j + 2 < m) {
+      ans = Math.max(
+        ans,
+        a[i][j] + a[i][j + 1] + a[i][j + 2] + a[i + 1][j + 2]
+      );
+    }
+    // j
+    if (i + 2 < n && j + 1 < m) {
+      ans = Math.max(ans, a[i][j] + a[i + 1][j] + a[i + 2][j] + a[i][j + 1]);
+    }
+    // k
+    if (i + 2 < n && j + 1 < m) {
+      ans = Math.max(
+        ans,
+        a[i][j] + a[i][j + 1] + a[i + 1][j + 1] + a[i + 2][j + 1]
+      );
+    }
+    // l
+    if (i + 2 < n && j + 1 < m) {
+      ans = Math.max(
+        ans,
+        a[i][j] + a[i + 1][j] + a[i + 1][j + 1] + a[i + 2][j + 1]
+      );
+    }
+    // m
+    if (i + 2 < n && j + 1 < m) {
+      ans = Math.max(
+        ans,
+        a[i + 1][j] + a[i + 2][j] + a[i][j + 1] + a[i + 1][j + 1]
+      );
+    }
+    // n
+    if (i + 1 < n && j + 2 < m) {
+      ans = Math.max(
+        ans,
+        a[i + 1][j] + a[i][j + 1] + a[i + 1][j + 1] + a[i][j + 2]
+      );
+    }
+    // o
+    if (i + 1 < n && j + 2 < m) {
+      ans = Math.max(
+        ans,
+        a[i][j] + a[i][j + 1] + a[i + 1][j + 1] + a[i + 1][j + 2]
+      );
+    }
+    // p
+    if (i + 1 < n && j + 2 < m) {
+      ans = Math.max(
+        ans,
+        a[i][j + 1] + a[i + 1][j] + a[i + 1][j + 1] + a[i + 1][j + 2]
+      );
+    }
+    // q
+    if (i + 1 < n && j + 2 < m) {
+      ans = Math.max(
+        ans,
+        a[i][j] + a[i][j + 1] + a[i][j + 2] + a[i + 1][j + 1]
+      );
+    }
+    // r
+    if (i + 2 < n && j + 1 < m) {
+      ans = Math.max(
+        ans,
+        a[i][j + 1] + a[i + 1][j] + a[i + 1][j + 1] + a[i + 2][j + 1]
+      );
+    }
+    // s
+    if (i + 2 < n && j + 1 < m) {
+      ans = Math.max(
+        ans,
+        a[i][j] + a[i + 1][j] + a[i + 2][j] + a[i + 1][j + 1]
+      );
+    }
+  }
 }
 
 console.log(ans);
-
-function isBroken(c) {
-  return c
-    .toString()
-    .split("")
-    .some((v) => brokenNumber.includes(v));
-}
